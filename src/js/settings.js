e73438887bda6e2aac621641dabe0d139d7cbc36
Anna Kuszal <anna.kuszal@o2.pl>
2019-12-16 23:12:40 +0100

    Add settings.js

---
+++ src/js/settings.js
@@ -0,0 +1,22 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    nav: '.sidebar',
+  },
+  
+  nav: {
+    links: '.sidebar a',
+  },
+  
+};
+  
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  
+  pages: {
+    active: 'active',
+  },
+  
+};
